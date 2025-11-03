905c3b4e9a3312b22e9f84aff128628d6857ed41
fuyong7335 <yukie353yukie@gmail.com>
2025-11-03 22:20:35 +0900

    Add files via upload

---
+++ okataduke-emotionmain_emotion.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("💓 感情ためこみタイプのあなたへ")
+st.write("💡 思い出や感情が手放しにくいあなたに合ったステップをお伝えします。")
+st.markdown("▼アドバイスの続きは、無料メール講座でお届けしています👇")
+st.markdown("[📩 メールで受け取る（無料）](https://www.reservestock.jp/subscribe/221907)")
